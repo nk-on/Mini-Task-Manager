efdd5b52b594c7486f5a0d01f97268d86d7222ce
nk-on <2nkoz@proton.me>
2026-03-02 22:11:42 +0400

    Feat:adding css file to the home.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const home = require('./routes/home')
+const home = require('./routes/home');
+const path = require('path')
+app.use(express.static(path.join(__dirname,'public')))
 app.use(home)
 app.listen(PORT);
