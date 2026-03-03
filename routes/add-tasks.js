c8f99ea2a807ef7b65447cd12d1c9bc0679ba9e3
nk-on <2nkoz@proton.me>
2026-03-03 13:41:52 +0400

    changing id generating logic

--- routes/add-tasks.js
+++ routes/add-tasks.js
@@ -2,22 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path  = require('path');
 const tasks = [];
-function uniqueIdGenerator() {
-    let id = Math.floor(Math.random()*100000);
-    tasks.forEach((task) => {
-        if (task.id === id) {
-            id = Math.floor(Math.random()*100000);
-        }
-    });
-    return id;
-}
+let id = 1;
 router.get('/add-tasks', (req, res) => {
     res.sendFile(path.join(__dirname,'../','views/add-tasks.html'))
 });
 router.post('/task', (req, res) => {
     const {task} = req.body;
     tasks.push({
-        taskId:uniqueIdGenerator(),
+        taskId:id++,
         task
     })
     console.log(tasks)
