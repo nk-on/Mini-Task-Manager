b0a828fc0d3948d9f149bba67cf1b52e75b0f3ec
nk-on <2nkoz@proton.me>
2026-03-03 22:42:15 +0400

    Feat:displaying already added tasks

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const PORT = 3000;
 const home = require('./routes/home');
 const addTasks = require('./routes/add-tasks')
+const viewTasks = require('./routes/view-tasks')
 const path = require('path');
 app.set('view engine', 'ejs'); 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
 app.use(home);
-app.use(addTasks)
+app.use(addTasks.router)
+app.use(viewTasks)
 app.listen(PORT);
